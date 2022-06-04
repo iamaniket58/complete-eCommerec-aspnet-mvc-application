370c59a22c2eae343b7081e2db10d3e667766bdb
iamaniket58 <iamaniket58@gmail.com>
2022-06-05 01:48:33 +0530

    Added Migrations

---
+++ eTickets/Migrations/20220604185039_NameFixPrice.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class NameFixPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "Movie",
+                newName: "Price");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Movie",
+                newName: "price");
+        }
+    }
+}
