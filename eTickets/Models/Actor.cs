0e11a6675c5434fb9cd2f10c8850eb9e701953b7
iamaniket58 <iamaniket58@gmail.com>
2022-06-05 01:55:37 +0530

    comment

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -14,7 +14,7 @@ namespace eTickets.Models
         public string FullName { get; set; }
         public string Bio { get; set; }
 
-        //Relationship
+        //Relationship/
         public List<Actor_Movie> Actors_Movies { get; set; }
     }
 }
